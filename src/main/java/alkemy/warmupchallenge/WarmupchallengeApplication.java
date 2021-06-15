a8b5a7a9df97c52bce187251a03fca82703a607c
Gabriel Andino <gabriel_andino@outlook.com>
2021-06-15 10:16:51 -0300

    Adding warm up challenge to github

---
+++ src/main/java/alkemy/warmupchallenge/WarmupchallengeApplication.java
@@ -0,0 +1,13 @@
+package alkemy.warmupchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmupchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmupchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alkemy/warmupchallenge/WarmupchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package alkemy.warmupchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmupchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
