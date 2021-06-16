6fae04e4991a5f7b009181f0f219edfedf657c0c
Gabriel Andino <gabriel_andino@outlook.com>
2021-06-16 14:32:42 -0300

    WUCA 0006 - Adding ErrorMessage, OperationStatusModel and Validation Errores, they are custom responses models

--- src/main/java/alkemy/warmupchallenge/models/requests/PostRequest.java
+++ src/main/java/alkemy/warmupchallenge/models/requests/PostRequest.java
@@ -1,13 +1,16 @@
 package alkemy.warmupchallenge.models.requests;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Data
+@Builder
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostRequest {
 
     @NotBlank(message = "Title is required.")
@@ -21,8 +24,5 @@ public class PostRequest {
 
     @NotNull(message = "Post category is required.")
     private long category;
-
-    @NotNull(message = "Post creator is required.")
-    private long user;
 
 }
--- src/main/java/alkemy/warmupchallenge/models/requests/UserLoginRequest.java
+++ src/main/java/alkemy/warmupchallenge/models/requests/UserLoginRequest.java
@@ -3,11 +3,15 @@ package alkemy.warmupchallenge.models.requests;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter @Setter
 public class UserLoginRequest {
 
+    @NotBlank(message = "Email is required.")
     private String email;
 
+    @NotBlank(message = "Password is required.")
     private String password;
 
 }
