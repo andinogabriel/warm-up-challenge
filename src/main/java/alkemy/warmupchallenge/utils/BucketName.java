6f4cf43e0752df55d29052aa7b123eeff7c04cee
Gabriel Andino <gabriel_andino@outlook.com>
2021-06-18 09:43:10 -0300

    WUCA 0015 - Adding a utils called bucketname

---
+++ src/main/java/alkemy/warmupchallenge/utils/BucketName.java
@@ -0,0 +1,11 @@
+package alkemy.warmupchallenge.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum BucketName {
+    ITEM_IMAGE("java-alkemy-challenge");
+    private final String bucketName;
+}
